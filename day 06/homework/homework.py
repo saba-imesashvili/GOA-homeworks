32bf2ad17639ce071ecd3c6531bac1db222c0a0a
saba-imesashvili <imesashvilisaba7@gmail.com>
2025-07-23 18:20:24 +0400

    commit

---
+++ day 06/homework/homework.py
@@ -0,0 +1,47 @@
+# boolean არის ერთ-ერთი მონაცემთა ტიპი რომელსაც გააჩნია ორი მნიშვნელობა True და False
+
+
+# integer 
+print(2)
+print(7)
+print(90)
+print(22)
+print(17)
+
+# string
+print("saba")
+print("luka")
+print("leqso")
+print("lomi")
+print("vefxvi")
+
+# float
+print(2.5)
+print(5.5)
+print(15.5)
+print(10.5)
+print(30.5)
+
+# boolean
+print(True)
+print(False)
+print(True)
+print(False)
+print(True)
+
+
+number1 = int(input("Enter Number1:"))
+number2 = int(input("Enter number2:"))
+print(number1 < number2)
+print(number1 > number2)
+print(number1 >= number2)
+print(number1 <= number2)
+print(number1 == number2)
+print(number1 != number2)
+
+
+# True და False
+# ჩართვა და გამორთვა
+# 1 და 0
+# დადებითი და უარყოფითი
+# ჭეშმარიტი და მცდარი
